b56ba15043243f63a388a478334639d06b36bb55
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-23 23:20:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
 }
 
-function setBestCustomer {
-  var bestCustomer = 'not bob';
+function setBestCustomer() {
+  bestCustomer = 'not bob';
 }
